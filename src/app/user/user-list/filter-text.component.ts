bb93796cccc50da06ca893dd20d09322ace5f482
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-09-30 16:08:30 -0300

    creates filtertext component

---
+++ src/app/user/user-list/filter-text.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-text',
+  templateUrl: './filter-text.component.html',
+})
+export class FilterTextComponent implements OnInit {
+    constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserComponent } from './user.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { FilterTextComponent } from './user-list/filter-text.component';
 
 @NgModule({
-  declarations: [UserComponent, UserListComponent],
+  declarations: [UserComponent, UserListComponent, FilterTextComponent],
   imports: [CommonModule],
-  exports: [UserComponent],
+  exports: [UserComponent, UserListComponent, FilterTextComponent],
 })
 export class UserModule {}
